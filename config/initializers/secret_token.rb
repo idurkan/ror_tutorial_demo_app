a57f36e13abadedd37322579c8c3ce6d53e37bb1
Ian Durkan <ian.durkan@gmail.com>
2012-01-23 00:27:46 -0500

    Commit ROR generated files and changed Gemfile.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '5dfad99e29a4d2f56af791f832562966111f570448e4bc9360fca7c52bd097f0ef9210f3bba287a0ebc3a76b639c4fa09c7a976b626b128c4120eb788bc03552'
